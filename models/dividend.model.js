1ce39cd99e3b6044e80ff025a05361416733b838
Northern-Web <kvikk92@gmail.com>
2022-05-13 01:45:06 +0200

    Added dividend model

---
+++ models/dividend.model.js
@@ -0,0 +1,53 @@
+const mongoose  = require('mongoose');
+const timestamp = require('mongoose-timestamp');
+
+var DividendSchema = new mongoose.Schema({
+  ticker: {
+    type:     String,
+    required: true,
+    trim:     true
+  },
+  transacted_units: {
+    type:     Number,
+    required: true
+  },
+  dividend_per_unit: {
+    type:     Number,
+    required: true
+  },
+  exchange_rate: {
+    type:     Number,
+    required: true
+  },
+  withholding_tax: {
+    type:     Number,
+    required: true
+  },
+  currency: {
+    type:     String,
+    required: true,
+    trim:     true
+  },
+  frequency: {
+    type:     Number,
+    required: true
+  },
+  share_price: {
+    type:     Number,
+    required: true
+  },
+  payment_date: {
+    type:     Date,
+    required: true
+  },
+  owner: {
+    type:     String,
+    required: true,
+    trim:     true
+  }
+});
+
+DividendSchema.plugin(timestamp);
+
+var Dividend = mongoose.model('Dividend', DividendSchema);
+module.exports = { Dividend };
