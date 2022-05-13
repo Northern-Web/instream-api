df897ff88aa77155e87ff5e13813812bee34eaa9
Michael Sagnes <michael@northernweb.no>
2022-05-13 15:11:57 +0200

    Finalized dividend CRUD operations in api.

--- controllers/dividend.controller.js
+++ controllers/dividend.controller.js
@@ -30,3 +30,88 @@ exports.create = async (req, res, next) => {
     });
   });
 };
+
+exports.delete = async (req, res, next) => {
+  const id = req.params.id;
+
+  const dividend = await Dividend.findById(id);
+  dividend.delete(() => {
+    return res.status(200).json({
+      "api": `${global.gConfig.app_desc} v.${global.gConfig.app_ver}`,
+      "code": 200,
+      "status": "Success",
+      "message": "Dividend was successfully deleted."
+    });
+  });
+};
+
+exports.patch = async (req, res, next) => {
+  const id   = req.params.id;
+  const data = req.body;
+
+  Dividend.findOneAndUpdate({ _id: id }, data).then(() => {
+    return res.status(200).json({
+      "api": `${global.gConfig.app_desc} v.${global.gConfig.app_ver}`,
+      "code": 200,
+      "status": "Success",
+      "message": "Dividend was successfully updated."
+    });
+  });
+};
+
+exports.find = async (req, res, next) => {
+  var options = {};
+  var query   = {};
+
+  options.limit = parseInt(req.query.limit) || 50;
+  options.skip  = parseInt(req.query.skip)  || 0;
+
+  const encoded_token = req.headers["x-access-token"];
+  var decoded_token   = jwt.verify(encoded_token, global.gConfig.jwt_secret);
+
+  if (req.query.orderBy) {
+    options.sort = req.query.orderBy;
+  }
+
+  if (req.query.order) {
+    if (req.query.order == "desc") {
+      options.sort = "-" + req.query.orderBy;
+    }
+  }
+
+  if (req.query.ticker) {
+    query["ticker"] = req.query.ticker;
+  }
+
+  if (req.query.currency) {
+    query["currency"] = req.query.currency;
+  }
+
+    // Ensures that the API only provides dividend data to the certain user.
+    query["owner"] = decoded_token.id;
+
+
+  Dividend.find(query)
+  .setOptions(options)
+  .exec(function (err, dividends) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        "api": `${global.gConfig.app_desc} v.${global.gConfig.app_ver}`,
+        "code": 500,
+        "status": "Server Error",
+        "message": "A problem occured when fetching dividends."
+      });
+    } else {
+      return res.status(200).json({
+        "api": `${global.gConfig.app_desc} v.${global.gConfig.app_ver}`,
+        "code": 200,
+        "status": "Success",
+        "filters": options,
+        "documents": dividends.length,
+        "data": dividends
+      });
+    }
+  })
+
+};
--- routes/dividend.routes.js
+++ routes/dividend.routes.js
@@ -5,6 +5,8 @@ const dividendController = require("./../controllers/dividend.controller");
 const router = express.Router();
 
 router.post("/", authJwt.verifyToken, dividendController.create);
-//router.get("/", authJwt.verifyToken, stockController.get);
+router.get("/", authJwt.verifyToken, dividendController.find);
+router.patch("/:id", authJwt.verifyToken, dividendController.patch);
+router.delete("/:id", authJwt.verifyToken, dividendController.delete);
 
 module.exports = router;
