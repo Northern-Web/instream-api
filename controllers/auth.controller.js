d3693123316d7de8a9cbfcc97a83601e5a9ea553
Michael Sagnes <kvikk92@gmail.com>
2023-08-30 23:41:02 +0200

    Replaced legacy environmental variable

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -70,7 +70,7 @@ exports.signin = (req, res) => {
     }
 
     var token = jwt.sign({ id: user.id, },
-    global.gConfig.jwt_secret, {
+    process.env.JWT_SECRET, {
       expiresIn: 86400 // 24 hours
     });
 
